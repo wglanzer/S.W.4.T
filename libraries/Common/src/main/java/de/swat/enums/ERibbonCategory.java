a3c2e901f24105949eb620b8704cf24d7a189383
w.glanzer <wernerglanzer@googlemail.com>
2014-02-22 01:47:32 +0100

    ERibbonCategory nun mit position und displayName

--- libraries/Common/src/main/java/de/swat/enums/ERibbonCategory.java
+++ libraries/Common/src/main/java/de/swat/enums/ERibbonCategory.java
@@ -5,6 +5,28 @@ package de.swat.enums;
  */
 public enum ERibbonCategory
 {
-  Components,
-  Anderes
+  COMMON("Common", 100),
+  Components("Components", 200);
+
+  /**
+   * Deklaration der Enums
+   */
+  private final String displayName;
+  private final int position;
+
+  private ERibbonCategory(final String pDisplayName, final int pPosition)
+  {
+    displayName = pDisplayName;
+    position = pPosition;
+  }
+
+  public int getPosition()
+  {
+    return position;
+  }
+
+  public String getDisplayName()
+  {
+    return displayName;
+  }
 }
