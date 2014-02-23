7a59293483c69c92f8ced15ef9394537d5948588
w.glanzer <wernerglanzer@googlemail.com>
2014-02-23 01:02:38 +0100

    NewFileAction

---
+++ libraries/DataModels/src/main/java/de/swat/ribbonactions/NewFileAction.java
@@ -0,0 +1,62 @@
+package de.swat.ribbonactions;
+
+import de.swat.*;
+import de.swat.accesses.MapCreatorModelAccess;
+import de.swat.annotations.RibbonAction;
+import de.swat.constants.IRibbonConstants;
+import de.swat.enums.*;
+import de.swat.util.ImageUtil;
+import org.jetbrains.annotations.Nullable;
+import org.pushingpixels.flamingo.api.common.icon.ResizableIcon;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+/**
+ * @author W. Glanzer, 22.02.14
+ */
+@RibbonAction
+public class NewFileAction extends AbstractRibbonAction
+{
+  @Override
+  public void actionPerformed(ActionEvent pSourceEvent, JComponent pInvoker, IModelAccess pModelAccess)
+  {
+    if(pModelAccess instanceof MapCreatorModelAccess)
+    {
+      MapCreatorModelAccess modelAccess = (MapCreatorModelAccess) pModelAccess;
+      modelAccess.setMap(new Map());
+    }
+  }
+
+  @Nullable
+  @Override
+  public ResizableIcon getIcon()
+  {
+    return ImageUtil.loadResizableIcon(NewFileAction.class.getResource("document6.png"), IRibbonConstants.ICON_SIZE);
+  }
+
+  @Override
+  public String getTitle()
+  {
+    return "New...";
+  }
+
+  @Override
+  public int getPosition()
+  {
+    return 50;
+  }
+
+  @Override
+  public ERibbonCategory getCategory()
+  {
+    return ERibbonCategory.COMMON;
+  }
+
+  @Override
+  public ERibbonSubCategory getSubCategory()
+  {
+    return ERibbonSubCategory.FILE;
+  }
+
+}
