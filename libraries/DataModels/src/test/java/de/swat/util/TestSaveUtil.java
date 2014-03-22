8d44509c0a1b4ca8cb0748448506785243a755e2
w.glanzer <wernerglanzer@googlemail.com>
2014-03-23 00:10:26 +0100

    Neue ObservableList implementiert Serializable -> TestSaveUtil

--- libraries/DataModels/src/test/java/de/swat/util/TestSaveUtil.java
+++ libraries/DataModels/src/test/java/de/swat/util/TestSaveUtil.java
@@ -2,9 +2,12 @@ package de.swat.util;
 
 import de.swat.*;
 import de.swat.accesses.*;
+import de.swat.observableList2.ObservableList2;
 import org.junit.*;
 
+import java.awt.*;
 import java.io.*;
+import java.util.ArrayList;
 
 /**
  * Testet mit dem MapDataModel, ob das SaveUtil funktioniert.
@@ -21,8 +24,13 @@ public class TestSaveUtil
   @Test
   public void Test_SaveUtil() throws IOException
   {
+    ObservableList2<Point> collPoints = new ObservableList2<>();
+    collPoints.add(new Point(20, 20));
+    collPoints.add(new Point(30, 20));
+
     MapCreatorModelAccess mapModelAccess = new MapCreatorModelAccess();
     MapCreatorMap map = new MapCreatorMap(mapModelAccess);
+    map.setCollisionPoints(collPoints);
     mapModelAccess.setMapCreatorMap(map);
 
     File file = new File("libraries\\DataModels\\src\\test\\java\\de\\swat\\util\\SaveUtilTest.txt");
@@ -38,6 +46,7 @@ public class TestSaveUtil
     IModelAccess loaded = SaveUtil.load(file);
     MapCreatorModelAccess modelAccess = (MapCreatorModelAccess) loaded;
     MapCreatorMap map2 = modelAccess.getMapCreatorMap();
+    Assert.assertArrayEquals(collPoints.toArray(), map2.getCollisionPoints().toArray());
     Assert.assertEquals(map, map2);
   }
 
