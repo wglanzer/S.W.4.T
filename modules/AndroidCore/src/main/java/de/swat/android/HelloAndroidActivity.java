b5dd629b70f2de1f4ab0a432b845937aa1683c4b
w.glanzer <wernerglanzer@googlemail.com>
2014-04-05 11:16:43 +0200

    Modul lässt sich kompilieren, aber nicht auf den Emulator befördern. Deploy nicht möglich

---
+++ modules/AndroidCore/src/main/java/de/swat/android/HelloAndroidActivity.java
@@ -0,0 +1,16 @@
+package de.swat.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloAndroidActivity extends Activity
+{
+
+  @Override
+  public void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+  }
+
+}
+
