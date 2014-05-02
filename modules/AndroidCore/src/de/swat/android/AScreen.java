644a258b3d8324b7a7cbbb84298efc0e941c66d7
w.glanzer <wernerglanzer@googlemail.com>
2014-05-02 23:06:27 +0200

    Togglebutton für Server/P2P-Verbindung

--- modules/AndroidCore/src/de/swat/android/AScreen.java
+++ modules/AndroidCore/src/de/swat/android/AScreen.java
@@ -2,8 +2,13 @@ package de.swat.android;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import de.swat.android.constants.AIStaticConstants;
 import de.swat.android.dialog.ADialogDisplayer;
 import de.swat.android.gui.controls.TouchpadImpl;
 
@@ -17,7 +22,8 @@ import de.swat.android.gui.controls.TouchpadImpl;
 public class AScreen extends ScreenAdapter
 {
   private Stage stage = new Stage();
-  private float delt = 0f;
+  private TextButton enableWifi = new TextButton("enable Wifi", AIStaticConstants.SKIN_DEFAULT);
+  private Color background = new Color(255f, 255f, 0f, 1f);
 
   @Override
   public void show()
@@ -25,6 +31,30 @@ public class AScreen extends ScreenAdapter
     ADialogDisplayer.setStage(stage);
     Gdx.input.setInputProcessor(stage);
 
+    enableWifi.setBounds(stage.getWidth() - 100 - 5, stage.getHeight() - 40 - 5, 100, 40);
+    enableWifi.setChecked(true);
+    enableWifi.addListener(new ClickListener()
+    {
+      private boolean wasClickedBefore = false;
+
+      @Override
+      public void clicked(InputEvent event, float x, float y)
+      {
+        //Dann Server starten
+        if(!wasClickedBefore)
+        {
+          enableWifi.setText("disable Wifi");
+        }
+        else
+        {
+          enableWifi.setText("enable Wifi");
+        }
+
+        wasClickedBefore = !wasClickedBefore;
+      }
+    });
+
+    stage.addActor(enableWifi);
     stage.addActor(new TouchpadImpl().getTouchpad());
   }
 
@@ -37,12 +67,10 @@ public class AScreen extends ScreenAdapter
   @Override
   public void render(float delta)
   {
-    Gdx.gl.glClearColor((float) Math.sin(delt), (float) Math.sin(delt/2), (float) Math.sin(delt/4), 1f);
+    Gdx.gl.glClearColor(background.r, background.g, background.b, background.a);
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     stage.act(delta);
     stage.draw();
-
-    delt += delta;
   }
 
   @Override
